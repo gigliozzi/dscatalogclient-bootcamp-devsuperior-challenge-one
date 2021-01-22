19838e1f8c6d7af5d5c9ace41433df2941e0a70c
William Gigliozzi <gigliozzi@gmail.com>
2021-01-22 08:35:31 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalogclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dscatalogclient/services/ClientService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalogclient.entities.Client;
 import com.devsuperior.dscatalogclient.repositories.ClientRepository;
@@ -14,6 +15,7 @@ public class ClientService {
 	@Autowired
 	private ClientRepository clientRepository;
 	
+	@Transactional(readOnly = true)
 	public List<Client> findAll() {
 		return clientRepository.findAll();
 		
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TB_CLIENT (name,cpf,income,birth_date) VALUES ('Marlene Vieira','1234567890','12000.00','1994-11-05 11:15:30');
+INSERT INTO TB_CLIENT (name,cpf,income,birth_date) VALUES ('Ilene Vieira','1234567890','10000.00','1994-11-05 11:15:30');
+INSERT INTO TB_CLIENT (name,cpf,income,birth_date) VALUES ('Sebastião Paulo','1234567890','9000.00','1994-11-05 11:15:30');
