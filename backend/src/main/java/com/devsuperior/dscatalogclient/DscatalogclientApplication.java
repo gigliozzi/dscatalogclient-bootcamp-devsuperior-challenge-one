737e812597494cdd9bbf825700a335853839fb49
William Gigliozzi <gigliozzi@gmail.com>
2021-01-21 21:38:40 -0300

    Project Spring Boot created

---
+++ backend/src/main/java/com/devsuperior/dscatalogclient/DscatalogclientApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalogclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogclientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dscatalogclient/DscatalogclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalogclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
