aad72042a4694412fd6af13bcdb74f3b55c31029
William Gigliozzi <gigliozzi@gmail.com>
2021-01-22 21:41:29 -0300

    POST Update Client

--- backend/src/main/java/com/devsuperior/dscatalogclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dscatalogclient/services/ClientService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalogclient.dto.ClientDTO;
 import com.devsuperior.dscatalogclient.entities.Client;
 import com.devsuperior.dscatalogclient.repositories.ClientRepository;
-import com.devsuperior.dscatalogclient.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalogclient.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -28,7 +30,7 @@ public class ClientService {
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = clientRepository.findById(id);
-		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Client not found"));
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Client not found"));
 		return new ClientDTO(entity);
 	}
 
@@ -44,6 +46,23 @@ public class ClientService {
 		entity = clientRepository.save(entity);
 		
 		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = clientRepository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setCpf(dto.getCpf());
+			entity.setIncome(dto.getIncome());
+			entity.setBirthDate(dto.getBirthDate());
+			entity.setChildren(dto.getChildren());
+			entity = clientRepository.save(entity);
+			return new ClientDTO(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found => " + id);
+		}
 	}
 
 	
